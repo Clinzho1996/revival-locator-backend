3be842ffb77734b892177edd49fec65545ebbebf
Dev Clinton <devclinton@Devs-MacBook-Pro.local>
2026-05-05 15:34:18 +0100

    new pass

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -1,11 +1,13 @@
-import mongoose, { Schema, Document } from "mongoose";
+import mongoose, { Document, Schema } from "mongoose";
 
 export interface ICategory extends Document {
-  name: string;
+	name: string;
 }
 
 const CategorySchema = new Schema({
-  name: { type: String, required: true }
+	name: { type: String, required: true },
+	description: { type: String },
+	createdAt: { type: Date, default: Date.now },
 });
 
 export default mongoose.model<ICategory>("Category", CategorySchema);
